66068ee43ee1fa967c6aa1592c7c5f1332475af0
Saurav Kumar <99734122+Saurav676sau@users.noreply.github.com>
2023-06-16 08:42:09 +0530

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,1 +1,30 @@
+<?php
 
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form data
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  // Connect to the databases
+  $conn = mysqli_connect('localhost','root','','databases');
+
+  // Check if the connection was successful
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  // Prepare the SQL query
+  $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+  // Execute the query
+  if (mysqli_query($conn, $query)) {
+    echo "Record created successfully";
+  } else {
+    echo "Error: " . $query . "<br>" . mysqli_error($conn);
+  }
+
+  // Close the connection
+  mysqli_close($conn);
+}
+?>
